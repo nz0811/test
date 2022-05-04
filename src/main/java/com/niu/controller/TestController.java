cd86e68f26a330abf6b1a589519d14081de26c4d
v-niuzhe <v-niuzhe@xiaomi.com>
2022-05-04 17:29:21 +0800

    commit

--- src/main/java/com/niu/controller/TestController.java
+++ src/main/java/com/niu/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.niu.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
+
     @GetMapping("/test")
-    public String test(){
+    public String test(String a) {
+        log.info(" test controller {}", a);
         return "test1245645643";
     }
 }
